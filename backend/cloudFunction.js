5eab6ef653b6f0189be3d3e87518cbac3da8d841
DanarVirkan <danarvirkan@gmail.com>
2021-06-03 21:28:04 +0700

    added getClustering function to fetch data from cloud function

--- backend/cloudFunction.js
+++ backend/cloudFunction.js
@@ -3,7 +3,7 @@ const getSentiment = (comment) => {
     const body = {
         comment
     }
-    return fetch("https://asia-southeast2-capstone-b21-cap0156.cloudfunctions.net/text-predictions-public", {
+    return fetch('https://asia-southeast2-capstone-b21-cap0156.cloudfunctions.net/text-predictions-public', {
             method: 'POST',
             body: JSON.stringify(body),
             headers: {
@@ -20,4 +20,21 @@ const getSentiment = (comment) => {
         });
 }
 
-module.exports = { getSentiment }
+const getClustering = (reportCase) => {
+    const body = {
+        case: reportCase
+    }
+    return fetch('https://asia-southeast2-capstone-b21-cap0156.cloudfunctions.net/clustering-public', {
+            method: 'POST',
+            body: JSON.stringify(body),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(res => res.json())
+        .then(json => json.Group)
+        .catch(e => {
+            console.error(e)
+        });;
+}
+
+module.exports = { getSentiment, getClustering };
